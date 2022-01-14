37d9ec647a9d78163174c773684c70fcac26e53c
Alexander Titov <44509969+titoffag@users.noreply.github.com>
2022-01-14 20:09:59 +0400

    [big frontend dev] implement curry

---
+++ big-frontend-dev/001-curry.js
@@ -0,0 +1,11 @@
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    } else {
+      return function(args2) {
+        return curried.apply(this, args.concat(args2);
+      }
+    }
+  }  
+}
