fa88f8db463998b0e74f65b3b1c6fb5a14248dc3
Alexander Titov <44509969+titoffag@users.noreply.github.com>
2022-03-28 00:06:43 +0400

    Create 027-maximum-depth-of-binary-tree.ts

---
+++ leetcode-patterns/027-maximum-depth-of-binary-tree.ts
@@ -0,0 +1,12 @@
+import { TreeNode } from "000-tree-node";
+
+function maxDepth(node: TreeNode | null): number {
+    if (!node) {
+        return 0;
+    }
+    
+    const left = maxDepth(node.left);
+    const right = maxDepth(node.right);
+    
+    return Math.max(left, right) + 1;
+};
