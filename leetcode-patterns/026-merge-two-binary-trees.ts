b893219a0731c998dae9c66184b65059af39d442
Alexander Titov <44509969+titoffag@users.noreply.github.com>
2022-03-28 00:05:46 +0400

    Create 026-merge-two-binary-trees.ts

---
+++ leetcode-patterns/026-merge-two-binary-trees.ts
@@ -0,0 +1,17 @@
+import { TreeNode } from "000-tree-node";
+
+function mergeTrees(node1: TreeNode | null, node2: TreeNode | null): TreeNode | null {
+    if (!node1) {
+        return node2;
+    }
+    if (!node2) {
+        return node1;
+    }
+    
+    node1.val += node2.val;
+    
+    node1.left = mergeTrees(node1.left, node2.left);
+    node1.right = mergeTrees(node1.right, node2.right);
+    
+    return node1;
+};
