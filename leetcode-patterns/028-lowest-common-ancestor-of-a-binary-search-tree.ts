64ae8efdc51a8310db9efb4910633bf8bd44a95d
Alexander Titov <44509969+titoffag@users.noreply.github.com>
2022-03-28 00:07:47 +0400

    Create 028-lowest-common-ancestor-of-a-binary-search-tree.ts

---
+++ leetcode-patterns/028-lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -0,0 +1,17 @@
+import { TreeNode } from "000-tree-node";
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	let node = root;
+    
+    while (node) {
+        if (p.val > node.val && q.val > node.val) {
+            node = node.right;
+        } else if (p.val < node.val && q.val < node.val) {
+            node = node.left;
+        } else {
+            return node;
+        }
+    }
+    
+    return null;
+};
