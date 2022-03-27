44be7085b08af4828ef2fa59d6ae748ba248174c
Alexander Titov <44509969+titoffag@users.noreply.github.com>
2022-03-27 22:36:06 +0400

    Create 024-path-sum.ts

---
+++ leetcode-patterns/024-path-sum.ts
@@ -0,0 +1,15 @@
+import { TreeNode } from "000-tree-node";
+
+function hasPathSum(node: TreeNode | null, target: number): boolean {
+    if (!node) {
+        return false;
+    }
+    
+    target -= node.val
+    
+    if (!node.left && !node.right) {
+        return target === 0;
+    }
+    
+    return hasPathSum(node.left, target) || hasPathSum(node.right, target);
+};
