8ebad44fdc17ce2d03ec06d3c47b8ff6bd41ab51
Alexander Titov <44509969+titoffag@users.noreply.github.com>
2022-03-28 00:09:26 +0400

    Create 030-invert-binary-tree.ts

---
+++ leetcode-patterns/030-invert-binary-tree.ts
@@ -0,0 +1,11 @@
+import { TreeNode } from "000-tree-node";
+
+function invertTree(node: TreeNode | null): TreeNode | null {
+    if (!node) {
+        return null;
+    }
+    
+    [node.left, node.right] = [invertTree(node.right), invertTree(node.left)];
+    
+    return node;
+};
