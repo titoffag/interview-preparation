8ca405910e454eea3320c7b5a4709b47b5f24319
Alexander Titov <44509969+titoffag@users.noreply.github.com>
2022-04-01 18:15:05 +0400

    Update 001-curry.js

--- big-frontend-dev/problems/001-curry.js
+++ big-frontend-dev/problems/001-curry.js
@@ -1,11 +1,6 @@
 function curry(fn) {
-  return function curried(...args) {
-    if (args.length >= fn.length) {
-      return fn.apply(this, args);
-    } else {
-      return function (args2) {
-        return curried.apply(this, args.concat(args2));
-      }
-    }
-  }
+  return function curryInner(...args) {
+    if (args.length >= fn.length) return fn(...args);
+    return (...args2) => curryInner(...args, ...args2);
+  };
 }
