127cbe2d7ef06978c67aa1aead66044200eff4e0
Alexander Titov <44509969+titoffag@users.noreply.github.com>
2022-04-20 21:44:01 +0400

    Create 006-debounce.js

---
+++ big-frontend-dev/problems/006-debounce.js
@@ -0,0 +1,9 @@
+function debounce(func, wait) {
+  let timer;
+  return function debounced() {
+    // or ...args as arguments of debounced function     
+    clearTimeout(timer);
+    setTimeout(() => func.apply(this, arguments), wait);
+    // or just pass func.bind(this, args) to setTimeout as first argument     
+  }
+}
