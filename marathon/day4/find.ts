679986553bf276e517b02f1d36d527bb3c467da8
Alexander Titov <titoffag@gmail.com>
2023-02-10 14:18:43 +0300

    [day4]

--- marathon/day4/find.ts
+++ marathon/day4/find.ts
@@ -1,30 +1,10 @@
-function find(str, words) {
-  const res = [];
-  const canonicalStr = str.split('').sort().join('');
-  const canonicalWords = words.map(word => word.split('').sort().join(''));
-  
-  let i = 0, j = 0, k = 0, count = 0, total = canonicalStr.length;
-  while (j < canonicalWords.length) {
-    if (canonicalStr[k] == canonicalWords[j][i]) {
-      count++;
-      k++;
-    }
-    i++;
-    
-    if (i == canonicalWords[j].length - 1) {
-      j++;
-    }
-    
-    if (count == total) {
-      res.push(words[j]);
-      count = 0;
-      i = 0;
-      k = 0;
-      j++;
-    }
-  }
-  
-  return res;
+function isFuzzySubstr(substr: string, str: string) {
+  return ~str.search(new RegExp(substr.split('').join('(.*)')));
+}
+
+function find(substr: string, strs: string[]): string[] {
+  return strs
+    .filter(str => isFuzzySubstr(substr, str))
 }
 
 console.log(find('kbza', [
--- marathon/day4/sort.ts
+++ marathon/day4/sort.ts
@@ -1,1 +1,17 @@
+function sort(array: number[]): number[] {
+  const mul2 = array
+    .filter(item => item % 2 === 0)
+    // lexicographical default sort
+    .sort((a, b) => a - b);
+
+  let mul2Index = 0;
+  return array
+    .map((item, index) => {
+      if (item % 2 === 0) {
+        return mul2[mul2Index++];
+      }
+      return array[index];
+    });
+}
+
 console.log(sort([7, 1, 4, 2, 9, 8])); // [7, 1, 2, 4, 9, 8]
