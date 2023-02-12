6c7b351918869799e81b3fb1c467b11f50171895
Alexander Titov <titoffag@gmail.com>
2023-02-12 03:12:31 +0300

    [leetcode] merge and intersection intervals

---
+++ leetcode-patterns/interval-list-intersections.ts
@@ -0,0 +1,28 @@
+function intervalIntersection(firstList: number[][], secondList: number[][]): number[][] {
+  let i = 0,
+      j = 0;
+
+  const result: number[][] = [];
+  while (i < firstList.length && j < secondList.length) {
+      const [start1, end1] = firstList[i];
+      const [start2, end2] = secondList[j];
+
+      const startIntersection = Math.max(start1, start2),
+          endIntersection = Math.min(end1, end2);
+
+      if (startIntersection <= endIntersection) {
+          result.push([startIntersection, endIntersection]);
+      }
+
+      if (end1 < end2) {
+          i++;
+      } else if (end1 > end2) {
+          j++;
+      } else {
+          i++;
+          j++;
+      }
+  }
+
+  return result;
+};
---
+++ leetcode-patterns/merge-intervals.ts
@@ -0,0 +1,18 @@
+function merge(intervals: number[][]): number[][] {
+  // default - lexicographical sorting
+  const sortedIntervalsByStart = intervals.sort((a, b) => a[0] - b[0]);
+
+  const mergedIntervals = [sortedIntervalsByStart[0]];
+  for (const [start1, end1] of sortedIntervalsByStart) {
+      const lastMergedInterval = mergedIntervals[mergedIntervals.length-1];
+      const [_, end2] = lastMergedInterval ?? [];
+
+      if (end2 < start1) {
+          mergedIntervals.push([start1, end1]);
+      } else {
+          mergedIntervals[mergedIntervals.length-1][1] = Math.max(end2, end1);
+      }
+  }
+
+  return mergedIntervals;
+};
